2ff99dea491a27ad15aaaf4479b11442b04f2bc1
Manuel Bellersen <BellersenM@googlemail.com>
2015-03-14 12:32:15 +0100

    Initial code for SG and SG:S for Win, Lin, Mac
    --HG--
    branch : linux-mac

---
+++ sg_sgs_linux_source/opengl/particle_editor2/particleforces.cpp
@@ -0,0 +1,259 @@
+#include <boost/lexical_cast.hpp>
+
+#include "precompiled.h"
+
+// Copyright 2002-2004 Frozenbyte Ltd.
+
+#ifdef _MSC_VER
+#pragma warning( disable : 4800 )
+#endif
+
+#include <boost/shared_ptr.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <vector>
+#include <string>
+#include <map>
+#include <list>
+#include <fstream>
+#include <Storm3D_UI.h>
+#include "../editor/string_conversions.h"
+#include "../editor/parser.h"
+#include "track.h"
+//#include "paramblock.h"
+#include "particletiming.h"
+#include "parseutil.h"
+#include "particlesystem.h"
+#include "particleforces.h"
+
+using namespace frozenbyte::editor;
+
+namespace frozenbyte  {
+namespace particle {
+namespace {
+
+	int dragId = 1;
+	int gravityId = 2;
+	int windId = 3;
+	int sideGravityId = 4;
+
+} // unnamed
+
+// DRAG-----------------------------------------------------
+
+int DragParticleForce::getType()
+{
+	return dragId;
+}
+
+void DragParticleForce::setFactor(float f) {
+	m_factor = f;
+}
+	
+float DragParticleForce::getFactor() {
+	return m_factor;
+}
+	
+void DragParticleForce::preCalc(float t) {
+
+}
+
+void DragParticleForce::calcForce(Vector& force, const Vector& pos, const Vector& vel) {
+	float len = vel.GetLength();
+	Vector newVel = vel * (float)pow(1.0f - m_factor, len);
+	force = newVel - vel;
+}
+	
+void DragParticleForce::parseFrom(const editor::ParserGroup& pg) {
+	m_factor = convertFromString<float>(pg.getValue("factor", ""), 0.0f);
+}
+
+int DragParticleForce::getTypeId() const
+{
+	return dragId;
+}
+
+// GRAVITY -----------------------------------------------------
+
+int GravityParticleForce::getType()
+{
+	return gravityId;
+}
+
+void GravityParticleForce::setGravity(float f) {
+	m_gravity = f;
+}
+	
+float GravityParticleForce::getGravity() {
+	return m_gravity;
+}
+
+void GravityParticleForce::preCalc(float t) {
+
+}
+	
+void GravityParticleForce::calcForce(Vector& force, const Vector& pos, const Vector& vel) {
+	force.x = 0.0f;
+	force.y = -m_gravity * PARTICLE_TIME_SCALE;
+	force.z = 0.0f;
+}
+	
+void GravityParticleForce::parseFrom(const editor::ParserGroup& pg) {
+	m_gravity = convertFromString<float>(pg.getValue("gravity", ""), 0.0f);
+}
+
+int GravityParticleForce::getTypeId() const
+{
+	return gravityId;
+}
+
+// SIDE GRAVITY -----------------------------------------------------
+
+int SideGravityParticleForce::getType()
+{
+	return sideGravityId;
+}
+
+void SideGravityParticleForce::setGravity(float f) {
+	m_sideGravity = f;
+}
+	
+float SideGravityParticleForce::getGravity() {
+	return m_sideGravity;
+}
+
+void SideGravityParticleForce::preCalc(float t) {
+
+}
+	
+void SideGravityParticleForce::calcForce(Vector& force, const Vector& pos, const Vector& vel) {
+	force.x = 0.0f;
+	force.y = 0.0f;
+	force.z = -m_sideGravity * PARTICLE_TIME_SCALE;
+}
+	
+void SideGravityParticleForce::parseFrom(const editor::ParserGroup& pg) {
+	m_sideGravity = convertFromString<float>(pg.getValue("sidegravity", ""), 0.0f);
+}
+
+int SideGravityParticleForce::getTypeId() const
+{
+	return sideGravityId;
+}
+
+// WIND -----------------------------------------------------
+
+int WindParticleForce::getType()
+{
+	return windId;
+}
+
+void WindParticleForce::setWindEffectFactor(float f) 
+{
+	m_wind_effect_factor = f;
+}
+	
+float WindParticleForce::getWindEffectFactor() 
+{
+	return m_wind_effect_factor;
+}
+
+void WindParticleForce::setSpiralAmount(float f) 
+{
+	m_spiral_amount = f;
+}
+	
+float WindParticleForce::getSpiralAmount() 
+{
+	return m_spiral_amount;
+}
+
+
+// cached so we don't recalculate them constantly
+static float global_wind_angle_cos = cosf(45.0f*3.1415f/180.0f);
+static float global_wind_angle_sin = cosf(45.0f*3.1415f/180.0f);
+
+static bool wind_timer_changed = true;
+static float wind_timer_spiral_speed_cos = 0.0f;
+static float wind_timer_spiral_speed_sin = 0.0f;
+
+
+void WindParticleForce::setSpiralSpeed(float f) 
+{
+	m_spiral_speed = f;
+	wind_timer_spiral_speed_cos = cosf(wind_timer * m_spiral_speed);
+	wind_timer_spiral_speed_sin = sinf(wind_timer * m_spiral_speed);
+	wind_timer_changed = false;
+}
+	
+float WindParticleForce::getSpiralSpeed() 
+{
+	return m_spiral_speed;
+}
+
+void WindParticleForce::preCalc(float t) 
+{
+}
+
+
+void WindParticleForce::calcForce(Vector &force, const Vector &pos, const Vector &vel) 
+{
+	force.x = global_wind_velocity * global_wind_angle_cos;
+	force.y = 0.0f;
+	force.z = global_wind_velocity * global_wind_angle_sin;
+	force *= m_wind_effect_factor;
+
+	if (wind_timer_changed)
+	{
+		wind_timer_spiral_speed_cos = cosf(wind_timer * m_spiral_speed);
+		wind_timer_spiral_speed_sin = sinf(wind_timer * m_spiral_speed);
+		wind_timer_changed = false;
+	}
+
+	if(m_spiral_amount != 0.0f)
+	{
+	    force.x += m_spiral_amount * wind_timer_spiral_speed_cos;
+		force.z += m_spiral_amount * wind_timer_spiral_speed_sin;
+	}
+
+	force *= PARTICLE_TIME_SCALE;
+}
+
+
+
+void WindParticleForce::parseFrom(const editor::ParserGroup& pg) {
+	m_wind_effect_factor = convertFromString<float>(pg.getValue("wind_effect_factor", ""), 0.0f);
+	m_spiral_amount = convertFromString<float>(pg.getValue("spiral_amount", ""), 0.0f);
+	setSpiralSpeed(convertFromString<float>(pg.getValue("spiral_speed", ""), 0.0f));
+}
+
+
+float WindParticleForce::wind_timer = 0.0f;
+float WindParticleForce::global_wind_angle = 45.0f*3.1415f/180.0f;
+float WindParticleForce::global_wind_velocity = 5.0f;
+
+void WindParticleForce::setGlobalWindAngle(float angleDegrees)
+{
+	WindParticleForce::global_wind_angle = angleDegrees * 3.1415926f/180.0f;
+	global_wind_angle_cos = cosf(WindParticleForce::global_wind_angle);
+	global_wind_angle_sin = sinf(WindParticleForce::global_wind_angle);
+}
+
+void WindParticleForce::setGlobalWindVelocity(float velocity)
+{
+  WindParticleForce::global_wind_velocity = velocity;
+}
+
+void WindParticleForce::advanceWind(float seconds)
+{
+  WindParticleForce::wind_timer += seconds;
+
+  wind_timer_changed = true;
+}
+
+int WindParticleForce::getTypeId() const
+{
+	return windId;
+}
+
+} // particle
+} // frozenbyte
---
+++ sg_sgs_linux_source/opengl/system/Timer.cpp
@@ -0,0 +1,130 @@
+
+#include "precompiled.h"
+#include <SDL.h>
+
+// Copyright 2002-2004 Frozenbyte Ltd.
+
+#ifdef _MSC_VER
+#pragma warning(disable:4103)
+#pragma warning(disable:4786)
+#endif
+
+
+#if defined(_WIN32)
+#include <windows.h>
+#endif  // _WIN32 && __GNUC__
+
+
+#include "Timer.h"
+
+// TEMP!!
+#include <string.h>
+#include "Logger.h"
+
+#include "../util/Debug_MemoryManager.h"
+
+#define TIMER_FACTOR_MULTIPLIER 256
+#define TIMER_FACTOR_MULTIPLIER_SHIFT 8
+
+int Timer::currentTime = 0;
+int Timer::currentUnfactoredTime = 0;
+int Timer::factorTimeAdd = 0;
+int Timer::timeHaxSub = 0;
+int Timer::timeFactor = TIMER_FACTOR_MULTIPLIER;
+
+
+void Timer::init()
+{
+	SDL_Init(SDL_INIT_TIMER);
+	update();
+}
+
+
+void Timer::uninit()
+{
+	SDL_QuitSubSystem(SDL_INIT_TIMER);
+}
+
+
+int Timer::getTime()
+{
+  return currentTime;
+}
+
+
+int Timer::getCurrentTime()
+{
+	// TODO: apple
+
+#ifdef _WIN32
+	return timeGetTime();
+
+#else
+	// assume linux
+	return SDL_GetTicks();
+
+#endif
+
+}
+
+
+int Timer::getUnfactoredTime()
+{
+  return currentUnfactoredTime;
+}
+
+
+void Timer::setTimeFactor(float factor)
+{
+	float oldFactor = (float)timeFactor / TIMER_FACTOR_MULTIPLIER;
+	int oldFactorTimeAdd = factorTimeAdd;
+
+	// must use this rounded new factor, can't use the given accurate
+	// factor as that is not really used (would cause a cumulative error)
+	float newFactor = float(int(factor * TIMER_FACTOR_MULTIPLIER)) / TIMER_FACTOR_MULTIPLIER;
+
+	Sint64 curActualTime = SDL_GetTicks();
+	int newFactorTimeAdd = 
+		oldFactorTimeAdd + (int)((float)curActualTime * (oldFactor - newFactor));
+
+	//int foo = curActualTime;
+  //foo = ((foo * timeFactor) >> TIMER_FACTOR_MULTIPLIER_SHIFT);
+	//foo += factorTimeAdd;
+
+	factorTimeAdd = newFactorTimeAdd;
+	timeFactor = (int)(factor * TIMER_FACTOR_MULTIPLIER);
+
+	//int bar = curActualTime;
+  //bar = ((bar * timeFactor) >> TIMER_FACTOR_MULTIPLIER_SHIFT);
+	//bar += factorTimeAdd;
+
+	//char buf[256];
+	//sprintf(buf, "%d -> %d, %d, %d", foo, bar, oldFactorTimeAdd, newFactorTimeAdd);
+	//Logger::getInstance()->error(buf);
+
+}
+
+float Timer::getTimeFactor()
+{
+	return timeFactor/(float)TIMER_FACTOR_MULTIPLIER;
+}
+
+
+void Timer::update()
+{
+  // NOTICE: presuming NT 10ms ticks!!! (not 95/98 55ms ticks)
+  // May well need a more accurate timer!
+  //currentTime = GetTickCount();
+	// fixed like this...
+	currentUnfactoredTime = SDL_GetTicks();
+	currentTime = currentUnfactoredTime;
+	currentTime = (int)(((Sint64)currentTime * (Sint64)timeFactor) >> TIMER_FACTOR_MULTIPLIER_SHIFT);
+	currentTime += factorTimeAdd;
+	currentTime -= timeHaxSub;
+}
+
+void Timer::addTimeSub(int time)
+{
+	timeHaxSub += time;
+}
+
